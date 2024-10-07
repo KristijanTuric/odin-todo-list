4bc84bf2c9f07273b987f74a6d48f1daac36ca3b
KristijanTuric <kristijan.turic1@gmail.com>
2024-10-07 21:58:03 +0200

    Basic header and footer, website structure

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import "./style.css";
+import { Task } from "./task";
 
+// Console testing
+const newTask = new Task("The Title", "This will describe your task", "1/1/1996", 1);
+
+
+//#region Footer
+
+const footerIcon = document.getElementById("footerIcon");
+footerIcon.addEventListener('click',() => {
+    window.open("https://github.com/KristijanTuric/odin-todo-list", '_blank').focus();
+})
+
+//#endregion
